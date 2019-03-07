9bb245ea707deaa04361108a5e16cec7c05e6314
cfs-14 <cfsandoval14@gmail.com>
2019-03-06 23:52:09 -0800

    Added empty typechecker

---
+++ src/main/java/CoopJa/TypeChecker.java
@@ -0,0 +1,20 @@
+package java.main.CoopJa;
+
+
+
+public class TypeChecker
+{
+    /*Get the list of 'declarations' in the program
+        +functionDefinitions
+        +variable Declarations
+        +other named types.
+    */
+    
+    /*Make a map of named types to their basic type or signature.
+    */
+    
+    /* Type check method either a bunch of typecheck methods
+        1 for each type, or a catch-all method.
+    */                   
+
+}
