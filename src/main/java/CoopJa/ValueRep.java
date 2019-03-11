d98b4cb32877c55fbd4c523fddb7db039b712f2f
JacobPoersch <jjpoersch21@gmail.com>
2019-03-11 04:24:13 -0700

    Hopefully committing this to the right branch

--- src/main/java/CoopJa/ValueRep.java
+++
@@ -1,9 +0,0 @@
-package main.java.CoopJa;
-
-import java.util.ArrayList;
-
-public class ValueRep {
-    public static ValueRep parseValueRep(ArrayList<Token> inputTokens, int start, int end){
-        return new ValueRep();
-    }
-}
