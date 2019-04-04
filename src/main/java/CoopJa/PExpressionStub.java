72cee552203281481fef29b8b06e4fbbebbf105a
nsa76330 <32834150+nsa76330@users.noreply.github.com>
2019-04-04 16:48:44 -0700

    merged changes & work with typechecker
    N_Typecheck_Test.java
    -merged Carlos' updates to getType() with current work
    -fixed issues with vardec, should be fairly complete (may add some small changes for methods)
    -continued work with method dec & its vars (still need PStatement stuff)
    
    PExpressionStub.java
    -added a comment to reflect that this class is obselete and totally unused
    -it was originally used for testing, but now is not necessary (may delete in future)

--- src/main/java/CoopJa/PExpressionStub.java
+++ src/main/java/CoopJa/PExpressionStub.java
@@ -1,6 +1,6 @@
 package CoopJa;
 
-public class PExpressionStub implements  PExpression {
+public class PExpressionStub implements  PExpression { //unused, just temp stuff
     Token stubToken;
     public PExpressionStub(Token stubToken){
         this.stubToken = stubToken;
