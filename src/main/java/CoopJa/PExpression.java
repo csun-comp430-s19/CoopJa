058cb781012b87043d1bfe4d86010509cd385c00
JacobPoersch <jjpoersch21@gmail.com>
2019-04-06 18:56:40 -0700

    Code Gen Test Case

--- src/main/java/CoopJa/PExpression.java
+++ src/main/java/CoopJa/PExpression.java
@@ -1,5 +1,7 @@
 package CoopJa;
 
+import java.util.ArrayList;
+
 public interface PExpression {
     //classes that implement this class
 
@@ -17,5 +19,16 @@ public interface PExpression {
 
     String generateString() throws CodeGenException;
 
- 
+    // TODO: REAL UNIT TESTS
+    public static void main(String[] args){
+        String foo = "2 + 16 / 4 * 3 + 1 - 6";
+        ArrayList<Token> fooToken = Token.tokenize(foo);
+        MainParser parsers = new MainParser();
+        PExpression fooTester2 = parsers.expressionLargeParser.parse(new TokenParserInput(fooToken)).getOrThrow();
+        try {
+            System.out.println(fooTester2.generateString());
+        } catch (CodeGenException e) {
+            e.printStackTrace();
+        }
+    }
 }
