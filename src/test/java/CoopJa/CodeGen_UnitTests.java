aa7fa4c9cb5c036dda63db1d0fdcfe27daa97609
Kaz-adr <akzadr@gmail.com>
2019-04-15 23:18:04 -0700

    Unit Testing Methods for new code generator. Still needs more tests

--- src/test/java/CoopJa/CodeGen_UnitTests.java
+++ src/test/java/CoopJa/CodeGen_UnitTests.java
@@ -64,4 +64,100 @@ public class CodeGen_UnitTests {
         testCodeGen("38 / 3 * 2 - 5 / 2", "ExpressionTest", "22");
     }
 
+    // Unit tests for New Parser
+    public void TestNewCodeGen(String input, String expectedOutPut){
+        String givenOutput = ParseToProgramString(input);
+        Assertions.assertTrue(givenOutput.equals(expectedOutPut), "Output and Input Differ" +
+                "\nExpected: \n" + expectedOutPut + "\nGot: " + givenOutput);
+    }
+
+    public String ParseToProgramString(String inputString){
+        ArrayList<Token> tokenList = Token.tokenize(inputString);
+        Input<Token> tokenListInput = new TokenParserInput(tokenList);
+        MainParser parsers = new MainParser();
+
+        PProgram fooTester = parsers.programParser.parse(tokenListInput).getOrThrow();
+        try {
+            String programString = fooTester.generateProgramString();
+            System.out.printf("%s", programString);
+            return programString;
+        } catch (CodeGenException e) {
+            e.printStackTrace();
+            return "Failed To Generate Code";
+        }
+    }
+
+    @Test
+    public void NewFullTest(){
+        TestNewCodeGen("public class test{\n" +
+                "    public int moduloHack(int x, int n){\n" +
+                "        int p;\n" +
+                "        int q;\n" +
+                "        int m;\n" +
+                "        q = x/n;\n" +
+                "        p = q*n;\n" +
+                "        m = x - p;\n" +
+                "        return m;\n" +
+                "    }\n" +
+                "    \n" +
+                "    public int main(){\n" +
+                "        for (int i = 0; i <= 45; i = i + 1;){\n" +
+                "            if (moduloHack(i, 15) == 0){\n" +
+                "                println(\"FizzBuzz\");\n" +
+                "            } \n" +
+                "            else{\n" +
+                "                if (moduloHack(i, 3)){\n" +
+                "                    println(\"Fizz\");\n" +
+                "                }\n" +
+                "                else{\n" +
+                "                    if (moduloHack(i ,5)){\n" +
+                "                        println(\"Buzz\");\n" +
+                "                    }else{}\n" +
+                "                }\n" +
+                "            }\n" +
+                "        }\n" +
+                "        return 0;\n" +
+                "    }\n" +
+                "}"
+                ,"#include <stdio.h>\n" +
+                        "\n" +
+                        "struct test{\n" +
+                        "};\n" +
+                        "int test_moduloHack (struct test* this,int x,int n){\n" +
+                        "    int p;\n" +
+                        "    int q;\n" +
+                        "    int m;\n" +
+                        "    q = (x)/(n);\n" +
+                        "    p = (q)*(n);\n" +
+                        "    m = (x)-(p);\n" +
+                        "    return m;\n" +
+                        "}\n" +
+                        "int test_main (struct test* this){\n" +
+                        "    for(int i = 0;(i)<=(45);i = (i)+(1)){\n" +
+                        "    if ((test_moduloHack(this, i,15))==(0)){\n" +
+                        "    printf(\"%s\\n\", \"FizzBuzz\");\n" +
+                        "}\n" +
+                        "else{\n" +
+                        "    if (test_moduloHack(this, i,3)){\n" +
+                        "    printf(\"%s\\n\", \"Fizz\");\n" +
+                        "}\n" +
+                        "else{\n" +
+                        "    if (test_moduloHack(this, i,5)){\n" +
+                        "    printf(\"%s\\n\", \"Buzz\");\n" +
+                        "}\n" +
+                        "else{\n" +
+                        "};\n" +
+                        "};\n" +
+                        "};\n" +
+                        "}\n" +
+                        ";\n" +
+                        "    return 0;\n" +
+                        "}\n" +
+                        "int main(int argc, char** argv){\n" +
+                        "    struct test mainClass = {};\n" +
+                        "    return test_main(&mainClass);\n" +
+                        "}");
+    }
+
+
 }
